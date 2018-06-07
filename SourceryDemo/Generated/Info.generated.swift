f84ae781b32d8481987d0d6ea897c00c4c8326ce
Krzysztof Zablocki <krzysztof.zablocki@pixle.pl>
2018-06-07 15:06:27 +0200

    step 1.2: use sourcery to introspect project structure

---
+++ SourceryDemo/Generated/Info.generated.swift
@@ -0,0 +1,11 @@
+// Generated using Sourcery 0.13.1 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+/*
+This project consists of:
+
+- 1 enums
+- 2 structs
+- 2 classes
+- 6 protocols
+*/
