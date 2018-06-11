d6d21068252f156899f5250a977453ddb39b1a34
Krzysztof Zablocki <krzysztof.zablocki@pixle.pl>
2018-06-11 20:40:18 +0200

    step 3.3: implement basic property equality

--- SourceryDemo/Generated/AutoEquatable.generated.swift
+++ SourceryDemo/Generated/AutoEquatable.generated.swift
@@ -5,5 +5,11 @@
 
 extension ArticleWebViewSettings: Equatable {
   static func == (lhs: ArticleWebViewSettings, rhs: ArticleWebViewSettings) -> Bool {
+    guard lhs.appBackgroundColor == rhs.appBackgroundColor else { return false }
+    guard lhs.nativeAdsEnabled == rhs.nativeAdsEnabled else { return false }
+    guard lhs.initialNumberOfAdsToFetch == rhs.initialNumberOfAdsToFetch else { return false }
+    guard lhs.adFetchDelay == rhs.adFetchDelay else { return false }
+    guard lhs.intermediateValue == rhs.intermediateValue else { return false }
     return true
   }
+}
