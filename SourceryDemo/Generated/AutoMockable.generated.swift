8343b0ebcb0aaeeb20801e2e5959470fe0a11b64
Krzysztof Zablocki <krzysztof.zablocki@pixle.pl>
2018-06-20 18:35:07 +0100

    step 5.2: start iterating over data to create a skeleton to work with

--- SourceryDemo/Generated/AutoMockable.generated.swift
+++ SourceryDemo/Generated/AutoMockable.generated.swift
@@ -2,3 +2,11 @@
 // DO NOT EDIT
 
 // AutoMockable
+
+class VariablesProtocolMock {
+  var company: String?
+  var name: String
+  var age: Int
+  var kids: [String]
+  var universityMarks: [String: Int]
+}
